e4c0225aaf6a93d7f5f019cb70f9e2c479688211
Peter Evans <peter.evans@gmx.net>
2016-03-23 12:31:50 +0100

    Working version showing temperature and orientation

--- Examples/nearables/AccelerometerExample-Swift/AccelerometerExample-Swift/DetailViewController.swift
+++ Examples/nearables/AccelerometerExample-Swift/AccelerometerExample-Swift/DetailViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DetailViewController: UIViewController, ESTNearableManagerDelegate {
+class DetailViewController: UIViewController, ESTNearableManagerDelegate, ESTTriggerManagerDelegate {
     
     @IBOutlet weak var typeLabel: UILabel!
     @IBOutlet weak var orientationLabel: UILabel!
@@ -16,6 +16,11 @@ class DetailViewController: UIViewController, ESTNearableManagerDelegate {
     
     var nearable:ESTNearable!
     var nearableManager:ESTNearableManager!
+    let triggerManager = ESTTriggerManager()
+    var lastRssi: Int = 0
+    let warmer = "warmer"
+    let colder = "colder"
+    var warmCold: String = "-"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,20 +28,38 @@ class DetailViewController: UIViewController, ESTNearableManagerDelegate {
         nearableManager = ESTNearableManager()
         nearableManager.delegate = self
         nearableManager .startRangingForIdentifier(nearable.identifier)
-        
+        setUpTrigger()
         typeLabel.text = ESTNearableDefinitions.nameForType(nearable.type)
+    }
+    func setUpTrigger(){
+        self.triggerManager.delegate = self
+        let rule1 = ESTOrientationRule.orientationEquals(.HorizontalUpsideDown, forNearableType: .Bag)
+        let rule2 = ESTMotionRule.motionStateEquals(true, forNearableIdentifier: "5e5e250a298c3366") //chair
+        let trigger = ESTTrigger(rules: [rule1, rule2], identifier: "Tom the Trigger")
+        self.triggerManager.startMonitoringForTrigger(trigger)
+    }
+    func triggerManager(manager: ESTTriggerManager, triggerChangedState trigger: ESTTrigger) {
+        if trigger.identifier == "Tom the Trigger" && trigger.state {
+            print("Tom has moved")
+        } else {
+            print("TaTa")
+        }
+    }
+    func itsGettingNearer(nearable: ESTNearable) -> Bool {
+        print("lastRssi ",lastRssi, "currentRssi", nearable.rssi)
+        let ret = nearable.rssi > lastRssi
+        lastRssi = nearable.rssi
+        return ret
     }
     
     //MARK: - ESTNearableManager delegate
     
     func nearableManager(manager: ESTNearableManager, didRangeNearable nearable: ESTNearable) {
 //        temperatureLabel.text = NSString(format: "%.1f°C", nearable.temperature)
-        
         var orientationString:String
-        if nearable.isMoving == false
-        {
-            switch nearable.orientation
-            {
+        var currSysVer:String = UIDevice().systemVersion
+        if !nearable.isMoving {
+            switch nearable.orientation {
             case ESTNearableOrientation.Horizontal:
                 orientationString = "Sticker is on its back"
             case ESTNearableOrientation.HorizontalUpsideDown:
@@ -52,18 +75,27 @@ class DetailViewController: UIViewController, ESTNearableManagerDelegate {
             case ESTNearableOrientation.Unknown:
                 orientationString = "Sticker orientation unknown"
             }
-        }
-        else
-        {
+        } else {
             orientationString = "Sticker is moving"
         }
-        
         orientationLabel.text = orientationString
-        
-        self.accelerometerLabel.text = NSString(format: "x axis: %dmG \n y axis: %dmG \n z axis: %dmG \n",
-        nearable.xAcceleration,
-        nearable.yAcceleration,
-        nearable.zAcceleration) as String
+        if nearable.rssi != lastRssi {
+            if nearable.rssi > lastRssi {
+                warmCold = warmer
+            } else if nearable.rssi < lastRssi {
+                warmCold = colder
+            }
+        }
+        lastRssi = nearable.rssi
+        self.accelerometerLabel.text = String(format: "x axis: %dmG \n y axis: %dmG \n z axis: %dmG \n temp: %.1f°C \n rssi: %d \n zone: %d \n %@ \n",
+            nearable.xAcceleration,
+            nearable.yAcceleration,
+            nearable.zAcceleration,
+            nearable.temperature,
+            nearable.rssi,
+            nearable.zone().rawValue,
+            warmCold
+        )
     }
 }
 
